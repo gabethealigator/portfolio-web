fd1ecc3830e81e59266e94cc687a7ec74a106295
Gabriel Nunes <gabrielnunesinfor@gmail.com>
2024-12-09 12:20:03 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Container, Box } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Box borderRadius="lg" bg="red" m="5" p="2">
+          Oi! Sou o Gabriel, formado em Desenvolvimento de Sistemas na ETEC de
+          Sapopemba. Sou apaixonado por Sistemas baseados em Unix e
+          desenvolvimento de software.
+        </Box>
+      </Container>
+    </>
+  );
+}
+
+export default App;
